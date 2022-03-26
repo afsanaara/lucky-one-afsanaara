7861a5266f1af29a589b1430d6c8aa2b4b8618db
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-03-26 19:11:51 +0600

    change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
     return (
         <div>
             <h3>Props vs State</h3>
-            <p>Props can read only. Props Cannot be Modified.State changes can be asynchonous. State can be modified using this.setState</p>
+            <p>Props can read only. Props Cannot be Modified. State changes can be asynchonous. State can be modified using this.setState</p>
 
             <h3>How useState() works</h3>
-            <p>useState is a function that allows to have state variables in functional components. it stores the initial state value and returns current state value as a variable and another function updates this value.</p>
+            <p>useState is a function that allows to have state variables in functional components. it stores the initial state value. it returns current state value as a variable. And another function updates this value.</p>
         </div>
     );
 };
